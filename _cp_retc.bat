a083b55c2f68fb71c69a1416db8ca7e7739571bf
Toshikazu Matsumura <matutosi@gmail.com>
2023-04-10 02:06:22 +0900

    add qpdf

---
+++ _cp_retc.bat
@@ -0,0 +1,11 @@
+d:
+cd D:\matu\work\ToDo\retc\_book
+xcopy D:\matu\work\ToDo\retc\_book D:\matu\work\ToDo\blog\docs\retc /Y /E
+
+d:
+cd d:\matu\work\todo\blog
+git add .
+git commit -m "update"
+git pull
+git push
+pause
